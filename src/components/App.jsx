9fea56c2407b3f123e42b3a2fd8f09ba7dbf1a59
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-13 18:51:00 -0600

    button mouse over event implemented a button background color change using states and mouse over events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [btnBG, setBtnBG] = useState({ backgroundColor: "white" });
+
+  function mouseOverHandler() {
+    setBtnBG({ backgroundColor: "black" });
+  }
+
+  function mouseOutHandler() {
+    setBtnBG({ backgroundColor: "white" });
+  }
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onMouseOver={mouseOverHandler}
+        onMouseOut={mouseOutHandler}
+        style={btnBG}
+      >
+        Submit
+      </button>
     </div>
   );
 }
