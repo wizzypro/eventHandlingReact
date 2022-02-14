4935e06216684f290511c8a81d9dfc8dc1e0c0a2
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-13 19:18:49 -0600

    onchange and onclick event implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [mouseOverState, setMouseOverState] = useState(false);
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
 
   function mouseOverHandler() {
     setMouseOverState(true);
@@ -10,14 +12,29 @@ function App() {
   function mouseOutHandler() {
     setMouseOverState(false);
   }
+
+  function formHandler(event) {
+    setName(event.target.value);
+  }
+
+  function submitHandler() {
+    setDisplayName(name);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {displayName}</h1>
+      <input
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+        onChange={formHandler}
+      />
       <button
         onMouseOver={mouseOverHandler}
         onMouseOut={mouseOutHandler}
         style={{ backgroundColor: mouseOverState ? "black" : "white" }}
+        type="submit"
+        onClick={submitHandler}
       >
         Submit
       </button>
